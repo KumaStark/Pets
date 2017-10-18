640b79f2776f1eaf09f6a0026f9105178250f1bd
KumaStark <elastic48@msn.com>
2017-10-18 14:06:59 +0800

    At Company 2017/10/18

---
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -0,0 +1,22 @@
+package com.example.android.pets.data;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by KumaNikuko on 2017/10/17.
+ */
+
+public class PetDbHelper extends SQLiteOpenHelper {
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
